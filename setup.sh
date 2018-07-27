e52c1e8375ecdd960abf64f7260bfa50f92f4965
clpo13 <clpo13@gmail.com>
2018-07-27 14:33:02 -0700

    Use python virtualenv instead of systemwide pip

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+# install dist requirements
 sudo apt-get install python-software-properties
 sudo add-apt-repository -yn ppa:ubuntuhandbook1/apps # for mupdf, mupdf-tools
 sudo add-apt-repository -y ppa:malteworld/ppa        # for pdftk
-sudo apt-get install djvulibre-bin libdjvulibre21 libtiff-tools mupdf mupdf-tools pdftk poppler-utils git djview
+sudo apt-get install djvulibre-bin libdjvulibre21 libtiff-tools mupdf mupdf-tools pdftk poppler-utils git djview virtualenv
 
-sudo pip install --upgrade google-api-python-client
-sudo pip install clint requests wikitools poster oauth2client apiclient
-
-sudo pip uninstall -y  apiclient
-sudo pip uninstall -y google-api-python-client
-sudo pip install google-api-python-client
-sudo pip install gdcmdtools
+# install python requirements
+virtualenv --python=python2 venv
+source venv/bin/activate
+pip install -r requirements.txt
